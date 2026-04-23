5db9322c37714a23b6abb29e19bbe64f93390944
Yashkumar1160 <yk4442144@gmail.com>
2026-04-23 21:40:17 +0530

    [Yash Kumar] Configure application pipeline with JWT authentication, Swagger, CORS, and DI

---
+++ Program.cs
@@ -0,0 +1,111 @@
+using System.Text;
+using InkWell.Comment.Context;
+using InkWell.Comment.Repository.Interfaces;
+using InkWell.Comment.Repository.Repositories;
+using InkWell.Comment.Services.Interfaces;
+using InkWell.Comment.Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+// database
+builder.Services.AddDbContext<CommentDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("CommentDB"));
+});
+
+// dependency injection
+builder.Services.AddScoped<ICommentRepository, CommentRepositoryImpl>();
+builder.Services.AddScoped<ICommentService, CommentServiceImpl>();
+
+// jwt - same secret key as all other services
+byte[] keyBytes = Encoding.UTF8.GetBytes(configuration["Jwt:Secret"]);
+var securityKey = new SymmetricSecurityKey(keyBytes);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = securityKey,
+        ValidateIssuer = true,
+        ValidIssuer = configuration["Jwt:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = configuration["Jwt:Audience"],
+        ValidateLifetime = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAngular");
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CommentDbContext>();
+    db.Database.Migrate();
+}
+
+app.Run();
