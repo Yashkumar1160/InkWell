68cf41360f5c79c3488cd26fbec4d1538ec4b3f8
Yashkumar1160 <yk4442144@gmail.com>
2026-04-27 17:29:18 +0530

    [Yash Kumar] Configure application pipeline with JWT authentication, Swagger, CORS, and DI

---
+++ Program.cs
@@ -0,0 +1,126 @@
+using System.Text;
+using InkWell.Category.Context;
+using InkWell.Category.Repository.Interfaces;
+using InkWell.Category.Repository.Repositories;
+using InkWell.Category.Service.Interfaces;
+using InkWell.Category.Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuration
+var configuration = builder.Configuration;
+
+
+// Database service
+builder.Services.AddDbContext<CategoryDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("CategoryDB"));
+});
+
+
+// Dependency Injection
+builder.Services.AddScoped<ICategoryRepository, CategoryRepositoryImpl>();
+builder.Services.AddScoped<ICategoryService, CategoryServiceImpl>();
+
+// JWT (same as auth service so that token works accross all services)
+byte[] keyBytes = Encoding.UTF8.GetBytes(configuration["Jwt:Secret"]);
+var securityKey = new SymmetricSecurityKey(keyBytes);
+
+//Use JWT Bearer as the default method
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = securityKey,
+        ValidateIssuer = true,
+        ValidIssuer = configuration["Jwt:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = configuration["Jwt:Audience"],
+        ValidateLifetime = true
+    };
+});
+
+
+// Authorization
+builder.Services.AddAuthorization();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+// Add Controllers
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var app = builder.Build();
+
+
+// Middlewate Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAngular");
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+
+// Apply Migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CategoryDbContext>();
+    db.Database.Migrate();
+}
+
+app.Run();
